20d1fdd9e70dc67a1a82f6a4327164aee4d6f492
Romain <skryn01@gmail.com>
2015-05-08 03:19:29 +0200

    Creating a ConsoleAnalyzer

---
+++ zip-builder/src/main/java/org/heatup/builder/ConsoleAnalyzer.java
@@ -0,0 +1,35 @@
+package org.heatup.builder;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Scanner;
+
+/**
+ * Created by romain on 07/05/2015.
+ */
+@RequiredArgsConstructor
+public class ConsoleAnalyzer {
+    private final Builder builder;
+    private final Scanner console;
+
+    Runnable runnable = new Runnable() {
+        public void run() {
+            String command = null;
+
+            System.out.println("Write the 'build' command to analyze & rebuild releases");
+
+            while((command = console.next()) != null) {
+                try {
+                    if(command.equalsIgnoreCase("build"))
+                        builder.build();
+                } catch(Exception e){}
+            }
+        }
+    };
+
+    public void start() {
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.start();
+    }
+}
