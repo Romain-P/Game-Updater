5a3dd5fd7c0ecca22c6fb6ad3edb79f6cab424b5
Romain <skryn01@gmail.com>
2015-05-08 03:20:34 +0200

    Improving OsCheck

--- updater-api/src/main/java/org/heater/api/utils/OsCheck.java
+++ updater-api/src/main/java/org/heater/api/utils/OsCheck.java
@@ -4,15 +4,10 @@ import java.util.Locale;
 
 public final class OsCheck {
     public enum OSType {
-        Windows("windows"), MacOS("mac"), Linux("linux"), Other("other"), All("all");
+        WINDOWS, MAC, LINUX, Other, ALL;
 
-        private String name;
-        private OSType(String name) {
-            this.name = name;
-        }
-
-        public final String toString() {
-            return name;
+        public String toString() {
+            return this.name().toLowerCase();
         }
     }
 
@@ -22,11 +17,11 @@ public final class OsCheck {
         if (detectedOS == null) {
             String os = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
             if ((os.indexOf("mac") >= 0) || (os.indexOf("darwin") >= 0)) {
-                detectedOS = OSType.MacOS;
+                detectedOS = OSType.MAC;
             } else if (os.indexOf("win") >= 0) {
-                detectedOS = OSType.Windows;
+                detectedOS = OSType.WINDOWS;
             } else if (os.indexOf("nux") >= 0) {
-                detectedOS = OSType.Linux;
+                detectedOS = OSType.LINUX;
             } else {
                 detectedOS = OSType.Other;
             }
