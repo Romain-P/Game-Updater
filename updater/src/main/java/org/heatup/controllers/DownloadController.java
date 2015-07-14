16df985958cd1c5ad02fa0e1eff83a36b2a743d6
Romain <skryn01@gmail.com>
2015-07-14 11:11:47 +0200

    Finishing download updates & unzipping of files

--- updater/src/main/java/org/heatup/controllers/DownloadController.java
+++ updater/src/main/java/org/heatup/controllers/DownloadController.java
@@ -1,9 +1,15 @@
 package org.heatup.controllers;
 
+import lombok.SneakyThrows;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.FileHeader;
+import net.lingala.zip4j.model.UnzipParameters;
 import org.heatup.api.UI.AppManager;
 import org.heatup.api.controllers.Controller;
 import org.heatup.api.serialized.SerializedObject;
 import org.heatup.api.serialized.implementations.SerializedObjectImpl;
+import org.heatup.core.Main;
+import org.heatup.utils.AppUtils;
 import org.heatup.utils.FileUtils;
 
 import java.io.File;
@@ -25,7 +31,7 @@ import java.util.concurrent.TimeUnit;
 public class DownloadController implements Controller{
     private final AppManager manager;
     private final ReleaseController releases;
-    private Future<?> future;
+    private Future<?> future, progressFuture;
     private final SerializedObject<List<Integer>> serializedZips;
 
     public DownloadController(AppManager manager, ReleaseController releases) {
@@ -40,83 +46,142 @@ public class DownloadController implements Controller{
         this.future = manager.getWorker().submit(new Runnable() {
             @Override
             public void run() {
-                LinkedBlockingDeque<URL> files = releases.getFiles();
-                int increment = 1;
-                long downloaded = 0;
+                download();
+                unzipall();
 
-                //while(true) {
-                    try {
-                        //URL url = files.poll(1, TimeUnit.SECONDS);
-                        URL url = new URL("http://tpe-audition.net/carnaval.png");
+                manager.getForm().updateFinished();
+            }
+        });
+    }
 
-                        /**if(url == null && files.isEmpty()) {
-                            manager.getForm().updateFinished();
-                            break;
-                        }**/
+    private void download() {
+        LinkedBlockingDeque<URL> files = releases.getFiles();
+        int increment = 1;
+        long downloaded = 0;
 
-                        //if(url == null) continue;
+        while(true) {
+            try {
+                URL url = files.poll(1, TimeUnit.SECONDS);
 
-                        URLConnection connection = url.openConnection();
-                        File file = new File(url.getPath().substring(1));
-                        long length = file.length();
-
-                        //if(connection.getContentLengthLong() == file.length())
-                          //  continue;
-                        if(file.exists())
-                            connection.setRequestProperty("Range", "bytes=" + length + "-");
-
-                        FileChannel download = new FileOutputStream(file, file.exists()).getChannel();
-
-                        long totalBytes = connection.getContentLengthLong() + length, start = System.nanoTime(),
-                                time = System.nanoTime(), between, elapsed, data = 0;
-                        int percent, totalPercent;
-
-                        ReadableByteChannel channel = Channels.newChannel(connection.getInputStream());
-
-                        while(download.transferFrom(channel, file.length(), 1024) > 0) {
-                            between = System.nanoTime() - time;
-
-                            if(between < 1000000000) continue;
-
-                            length = file.length();
-                            elapsed = System.nanoTime() - start;
-
-                            percent = (int) ((double) ((double)length / ((double)totalBytes == 0.0 ? 1.0 : (double)totalBytes) * 100.0));
-                            totalPercent = (int) (((double)downloaded / (double)releases.getUpdateLength()) * 100.0);
-
-                            manager.getForm().updateCurrentPercentage(
-                                    percent,
-                                    increment,
-                                    files.size());
-
-                            manager.getForm().updateTotalPercentage(totalPercent,
-                                    FileUtils.getTimeAsString(((elapsed * totalBytes / length) - elapsed)/1000000),
-                                    FileUtils.getReadableSize(length - data));
-
-                            time = System.nanoTime();
-                            data = length;
-                        }
-
-                        download.close();
-                        channel.close();
-                        increment++;
-                    } catch(Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                if(url == null) {
+                    if(files.isEmpty()) {
+                        manager.getForm().updateFinished();
+                        break;
+                    } else continue;
                 }
 
-           // }
+                URLConnection connection = url.openConnection();
+                File file = new File(url.getPath().substring(1));
+                long length = file.length();
+
+                if(connection.getContentLengthLong() == file.length()) {
+                    downloadFinished(file);
+                    continue;
+                }
+                else if(file.exists())
+                    connection.setRequestProperty("Range", "bytes=" + length + "-");
+
+                FileChannel download = new FileOutputStream(file, file.exists()).getChannel();
+
+                long totalBytes = connection.getContentLengthLong() + length, start = System.nanoTime(),
+                        time = System.nanoTime(), between, elapsed, data = 0;
+                int percent, totalPercent;
+
+                ReadableByteChannel channel = Channels.newChannel(connection.getInputStream());
+
+                while(download.transferFrom(channel, file.length(), 1024) > 0) {
+                    between = System.nanoTime() - time;
+
+                    if(between < 1000000000) continue;
+
+                    length = file.length();
+                    elapsed = System.nanoTime() - start;
+
+                    percent = (int) ((double) ((double)length / ((double)totalBytes == 0.0 ? 1.0 : (double)totalBytes) * 100.0));
+                    totalPercent = (int) (((double)downloaded / (double)releases.getUpdateLength()) * 100.0);
+
+                    manager.getForm().updateCurrentPercentage(
+                            percent,
+                            increment,
+                            files.size());
+
+                    manager.getForm().updateTotalPercentage(totalPercent,
+                            FileUtils.getTimeAsString(((elapsed * totalBytes / length) - elapsed)/1000000),
+                            FileUtils.getReadableSize(length - data));
+
+                    time = System.nanoTime();
+                    data = length;
+                }
+
+                downloadFinished(file);
+
+                download.close();
+                channel.close();
+                increment++;
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void downloadFinished(File file) {
+        int release = Integer.parseInt(file.getName().replace(".zip", ""));
+        releases.getSerializedRelease().setObject(release).write();
+    }
+
+    private int increment = 1;
+    private ZipFile zip;
+
+    @SneakyThrows
+    private void unzipall() {
+        final List<Integer> zips = serializedZips.get();
+
+        progressFuture = manager.getWorker().submit(new Runnable() {
+            @SneakyThrows
+            public void run() {
+                while(zips.size() >= increment) {
+                    Thread.sleep(10);
+
+                    if(zip == null) continue;
+
+                    manager.getForm().updateCompressPercentage(
+                            zip.getProgressMonitor().getPercentDone(),
+                            increment,
+                            zips.size()
+                    );
+                }
+            }
         });
 
-        unzipall();
-    }
+        for(int i: zips) {
+            File file = new File(FileUtils.path("updates", String.valueOf(i), ".zip"));
 
-    private void unzipall() {
+            if(!file.exists()) {
+                releases.getFiles().addLast(new URL(
+                        FileUtils.path(Main.SERVER, "releases", AppUtils.OS.toString(), i + ".zip")));
 
-    }
+                if(!progressFuture.isCancelled())
+                    progressFuture.cancel(true);
+
+                download();
+                return;
+            }
+
+            zip = new ZipFile(file);
+            zip.extractAll(new File("").getAbsolutePath());
+
+            zips.remove((Integer) i);
+            serializedZips.setObject(zips).write();
+            increment++;
+        }
+}
+
     @Override
     public void end() {
         if(future != null && !future.isCancelled())
             future.cancel(true);
+
+        if(progressFuture != null && !progressFuture.isCancelled())
+            progressFuture.cancel(true);
     }
 }
